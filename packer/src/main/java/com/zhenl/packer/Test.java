dd71dad35b6b942331671b90fe35ca70fe9f5ddc
Sasuke <zhenlin1993@gmail.com>
2019-01-03 14:19:24 +0800

    add makeJar

--- packer/src/main/java/com/zhenl/packer/Test.java
+++ packer/src/main/java/com/zhenl/packer/Test.java
@@ -13,35 +13,39 @@ public class Test {
     public static void main(String[] args) throws Exception {
         try {
             String srcFilePath = args[0];
-            String channelFilePath = args[1];
             File srcFile = new File(srcFilePath);
-            File channelFile = new File(channelFilePath);
-            FileInputStream channelStream = new FileInputStream(channelFile);
-            byte[] channelBytes = new byte[channelStream.available()];
-            channelStream.read(channelBytes);
-            String[] channels = new String(channelBytes).split("\\|");
-            String outDir = channelFile.getParentFile().getAbsolutePath();
-            if (args.length >= 3) {
-                outDir = args[2];
-                File dir = new File(outDir);
-                if (!dir.exists())
-                    dir.mkdirs();
-            }
-            String name = srcFile.getName();
-            name = name.substring(0, name.length() - 4);
-            for (String channel : channels) {
-                File file = new File(outDir, name + "_" + channel + ".apk");
-                FileInputStream in = new FileInputStream(srcFile);
-                FileOutputStream out = new FileOutputStream(file);
-                byte[] b = new byte[1024];
-                int n = 0;
-                while ((n = in.read(b)) != -1) {
-                    out.write(b, 0, n);
+            if (args.length > 1) {
+                String channelFilePath = args[1];
+                File channelFile = new File(channelFilePath);
+                FileInputStream channelStream = new FileInputStream(channelFile);
+                byte[] channelBytes = new byte[channelStream.available()];
+                channelStream.read(channelBytes);
+                String[] channels = new String(channelBytes).split("\\|");
+                String outDir = channelFile.getParentFile().getAbsolutePath();
+                if (args.length >= 3) {
+                    outDir = args[2];
+                    File dir = new File(outDir);
+                    if (!dir.exists())
+                        dir.mkdirs();
                 }
-                in.close();
-                out.close();
-                PackerCommon.writeChannel(file, channel);
-                System.out.println(PackerCommon.readChannel(file));
+                String name = srcFile.getName();
+                name = name.substring(0, name.length() - 4);
+                for (String channel : channels) {
+                    File file = new File(outDir, name + "_" + channel + ".apk");
+                    FileInputStream in = new FileInputStream(srcFile);
+                    FileOutputStream out = new FileOutputStream(file);
+                    byte[] b = new byte[1024];
+                    int n = 0;
+                    while ((n = in.read(b)) != -1) {
+                        out.write(b, 0, n);
+                    }
+                    in.close();
+                    out.close();
+                    PackerCommon.writeChannel(file, channel);
+                    System.out.println(PackerCommon.readChannel(file));
+                }
+            } else {
+                System.out.println(PackerCommon.readChannel(srcFile));
             }
         } catch (Exception e) {
             System.out.println("Usage");
